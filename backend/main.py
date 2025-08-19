36a1ec785cb861e2bbe0e92a872cb0f13e7e8548
Riad <riad_mammadov@yahoo.com>
2025-08-19 04:36:15 +0100

    add router and cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+from file import router as fileRouter
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,     
+    allow_credentials=True,
+    allow_methods=["*"],         
+    allow_headers=["*"],         
+)
+
+app.include_router(fileRouter)
