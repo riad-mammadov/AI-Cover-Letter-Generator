5182d5dc1941843cb13547446b150f5358158dcd
Riad <riad_mammadov@yahoo.com>
2025-10-06 14:11:27 +0100

    add prompt for gemini api

--- backend/prompt.py
+++ backend/prompt.py
@@ -28,5 +28,28 @@ def cover_letter_prompt(cv_text, job_desc):
     - Job Description: {job_desc}
 
     Ensure that you do not send anything but the cover letter - Your job is to create the cover letter and nothing else. Dont add any placeholder values,
-    Just start the cover letter with "Dear Hiring Manager".
+    Start the cover letter with "Dear Hiring Manager".
     Generate a complete cover letter using the information above."""
+
+def cv_review_prompt(cv_text):
+    return f"""
+    You are an expert career coach and recruiter. 
+    Review the following CV and return feedback. 
+    
+
+    CV TEXT:
+    {cv_text}
+
+    Your response must follow this schema, include nothing else apart from this:
+
+    Summary: One-paragraph overall impression of the CV.,
+    Strengths: List of specific strengths found in the CV.,
+    Weaknesses: List of specific weaknesses or missing elements.,
+    Recommendations: "Concrete, actionable advice to improve the CV.,
+    Score: 0-100
+
+    Rules:
+    - Keep the summary concise but insightful.
+    - Score should reflect how competitive this CV would be in the job market (100 = outstanding, 0 = very poor).
+    - No Bulletpoints, just new lines if needed.
+    """
