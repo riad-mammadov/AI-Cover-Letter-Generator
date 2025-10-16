d7ebc331ae86a27060d1159dd6c0d96a56984d84
Riad <riad_mammadov@yahoo.com>
2025-10-16 15:57:28 +0100

    revert origin back to deployed url

--- backend/main.py
+++ backend/main.py
@@ -7,12 +7,11 @@ app = FastAPI()
 origins = [
     "https://ai-cover-letter-generator-khaki.vercel.app",
     
-    
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],     
+    allow_origins=origins,     
     allow_credentials=True,
     allow_methods=["*"],         
     allow_headers=["*"],         
